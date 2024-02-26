5e3b0b7c303171eca62af13f6cfdc47f1b9fb061
Ergothkampfgaist94 <67721909+Ergothkampfgaist94@users.noreply.github.com>
2024-02-25 22:40:25 -0500

    Add files via upload

---
+++ punto1/AppFormPedido/Cafetería Mi U/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Cafetería_Mi_U
+{
+    partial class Contadordejamones
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cheBoxAdQuin = new System.Windows.Forms.CheckBox();
+            this.cheBoxAdFest = new System.Windows.Forms.CheckBox();
+            this.lblCant = new System.Windows.Forms.Label();
+            this.cmbDia = new System.Windows.Forms.ComboBox();
+            this.lblVrAPagar = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Día";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(12, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cantidad";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cheBoxAdQuin
+            // 
+            this.cheBoxAdQuin.Location = new System.Drawing.Point(118, 131);
+            this.cheBoxAdQuin.Name = "cheBoxAdQuin";
+            this.cheBoxAdQuin.Size = new System.Drawing.Size(97, 31);
+            this.cheBoxAdQuin.TabIndex = 5;
+            this.cheBoxAdQuin.Text = "Adición Quincena";
+            this.cheBoxAdQuin.UseVisualStyleBackColor = true;
+            this.cheBoxAdQuin.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // cheBoxAdFest
+            // 
+            this.cheBoxAdFest.Location = new System.Drawing.Point(15, 132);
+            this.cheBoxAdFest.Name = "cheBoxAdFest";
+            this.cheBoxAdFest.Size = new System.Drawing.Size(97, 30);
+            this.cheBoxAdFest.TabIndex = 6;
+            this.cheBoxAdFest.Text = "Adición Festivos";
+            this.cheBoxAdFest.UseVisualStyleBackColor = true;
+            this.cheBoxAdFest.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // lblCant
+            // 
+            this.lblCant.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblCant.Location = new System.Drawing.Point(118, 92);
+            this.lblCant.Name = "lblCant";
+            this.lblCant.Size = new System.Drawing.Size(121, 23);
+            this.lblCant.TabIndex = 7;
+            this.lblCant.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblCant.Click += new System.EventHandler(this.lblCant_Click);
+            // 
+            // cmbDia
+            // 
+            this.cmbDia.FormattingEnabled = true;
+            this.cmbDia.Location = new System.Drawing.Point(118, 45);
+            this.cmbDia.Name = "cmbDia";
+            this.cmbDia.Size = new System.Drawing.Size(121, 21);
+            this.cmbDia.TabIndex = 8;
+            this.cmbDia.SelectedIndexChanged += new System.EventHandler(this.cmbdia_SelectedIndexChanged);
+            // 
+            // lblVrAPagar
+            // 
+            this.lblVrAPagar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblVrAPagar.Location = new System.Drawing.Point(115, 165);
+            this.lblVrAPagar.Name = "lblVrAPagar";
+            this.lblVrAPagar.Size = new System.Drawing.Size(121, 23);
+            this.lblVrAPagar.TabIndex = 9;
+            this.lblVrAPagar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblVrAPagar.Click += new System.EventHandler(this.lblVrAPagar_Click);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(12, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Valor A Pagar";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(140, 210);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 11;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(27, 210);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 12;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // Contadordejamones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(251, 246);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblVrAPagar);
+            this.Controls.Add(this.cmbDia);
+            this.Controls.Add(this.lblCant);
+            this.Controls.Add(this.cheBoxAdFest);
+            this.Controls.Add(this.cheBoxAdQuin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Contadordejamones";
+            this.Text = "Compras de jamón";
+            this.Load += new System.EventHandler(this.Contadordejamones_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox cheBoxAdQuin;
+        private System.Windows.Forms.CheckBox cheBoxAdFest;
+        private System.Windows.Forms.Label lblCant;
+        private System.Windows.Forms.ComboBox cmbDia;
+        private System.Windows.Forms.Label lblVrAPagar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
+
---
+++ punto1/libRnComp/clsRnCaf/clsRnCaf/clsRnComp.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libRnCaf
+{
+    public class clsRnComp
+    {
+        private int intDia;
+        private float fltVrUnit;
+        private int intCantLbrs;
+        private bool boolCantLbrsQuinc;
+        private bool boolCantLbrsFest;
+        private string strError;
+
+        public clsRnComp()
+        {
+                intDia = 0;
+                fltVrUnit = 0;
+                intCantLbrs = 0;
+                boolCantLbrsQuinc = false;
+                boolCantLbrsFest = false;
+                strError = string.Empty;
+        }
+
+        public int Dia { set { intDia = value; } }
+        public float VrUnid { get{ return fltVrUnit; } }
+        public int CantLbrs { get {  return intCantLbrs; } }
+        public bool CantQuinc{ get { return boolCantLbrsQuinc; } }
+        public bool CantFest { get { return boolCantLbrsFest; } }
+
+        private bool leerArchivo()
+        {
+            try
+            {
+                string strPath = AppDomain.CurrentDomain.BaseDirectory + @"pedido.txt";
+                int intCantRg = 0, intDiaSem, intCantNorm, intCantQuinc, intCantFest;//archivos planos
+                string strLinea;//capturar  llinea a linea el archivo plano
+                string[] vectorLinea;//vector tipo cadena dinamico va hacer el split del archivo plano
+                intCantRg = File.ReadAllLines(strPath).Length;//validar si hay registro en el txt
+                if (intCantRg <= 0)
+                    return true;
+                StreamReader Archivo = new StreamReader(@strPath); //Objeto para leer el archivo 
+                while ((strLinea = Archivo.ReadLine()) != null)  //Leer línea * línea el archivo 
+                {                    
+                    vectorLinea = strLinea.Split('|');
+                    intDiaSem = Convert.ToInt32(vectorLinea[0]);
+                    intCantNorm = Convert.ToInt32(vectorLinea[2]);
+                    intCantQuinc = Convert.ToInt32(vectorLinea[3]);
+                    intCantFest= Convert.ToInt32(vectorLinea[4]);  //Promedio de descuento
+                    if (intDia == intDiaSem)
+                    {
+                        intCantLbrs = intCantNorm;
+                        if (boolCantLbrsQuinc)
+                        {
+                            intCantLbrs += intCantQuinc;
+                        }
+                        if (boolCantLbrsFest)
+                        {
+                            intCantLbrs += intCantFest;
+                        }
+                           //Porcentaje del descuento
+                        break;
+                    }
+                    //son los privados  intcod,intcant
+                }
+                Archivo.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                strError = ex.Message;
+                return false;
+            }
+        }
+    }
+}
